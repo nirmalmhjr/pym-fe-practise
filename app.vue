4bb57aaae27b87493c9ca6846acdf180381d2af3
nirmalmhjr <nirmalmhjr@gmail.com>
2024-06-10 10:18:37 +0545

    phone number login and toggle button

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtLayout>
+    <!-- <v-btn @click="toggleTheme">toggle theme</v-btn> -->
+    <v-switch inset @change="toggleTheme" ></v-switch>
+    <v-app>
+      <NuxtPage />
+    </v-app>
+
+  </NuxtLayout>
+</template>
+
+<script setup>
+  import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+// function toggleTheme () {
+//   theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+// }
+
+const isDarkTheme = ref(theme.global.current.value.dark)
+
+function toggleTheme(){
+  isDarkTheme.value= !isDarkTheme.value
+}
+
+watch(isDarkTheme, (newValue)=>{
+  theme.global.name.value = newValue ? 'light': 'dark'
+})
+</script>
